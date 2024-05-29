93480d8eb0d4aba817aac78a771cff0a92031ed8
JackFed <jack.fedurek@gmail.com>
2024-05-29 19:08:24 -0400

    Added some calc features

--- math.js
+++ math.js
@@ -1,3 +1,12 @@
+// Globals bad but for now
+let displayValue = "";
+
+let values = {
+    a: 0,
+    b: 0,
+    op: ""
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,7 +26,7 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
     switch (operator) {
         case "+":
             return add(a, b);
@@ -30,23 +39,64 @@ function operate(operator, a, b) {
     }
 }
 
-function changeDisplay(value) {
+function changeDisplay(button, value) {
     const display = document.querySelector(".display-value");
-    if (display.innerHTML === "0" || (value === "/") || (value === "*") || (value === "+") || (value === "-") 
-        || (display.innerHTML === "/") || (display.innerHTML === "*") || (display.innerHTML === "+") || (display.innerHTML === "-")) {
+
+    if (display.innerHTML === "0") {
         display.innerHTML = value;
+        displayValue = value;
+    } else if( (value === "/") || (value === "*") || (value === "+") || (value === "-") ) {
+        setValues(display.innerHTML, value);
     } else if (value === "C") {
         display.innerHTML = "0";
+        // reset operation tracking
+        values.a = 0;
+        values.b = 0;
+        values.op = "";
     } else {
         display.innerHTML = display.innerHTML.concat(value);
+        displayValue = display.innerHTML;
     }
+
+}
+
+function highlightOperator () {
+    return;
 }
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        // Add function to check if the button was an operator
-        changeDisplay(button.innerHTML);
+        changeDisplay(button, button.innerHTML);
     });
 })
 
+// Get the values and the operator
+// If it doesn't have any value for a, set a and the operator
+// if it does, set the value of b, perform operate.
+// Then set the new value of a to the result, and set the new operator to the give operator.
+function setValues(value, operator) {
+    if(values.a === 0) {
+        values.a = Number(value);
+        values.op = operator;
+    } else if (operator === "=") {
+        values.b = Number(value);
+        const result = operate(values.a, values. op, values.b);
+    } else {
+        values.b = Number(value);
+        const result = operate(values.a, values. op, values.b);
+        values.a = result;
+        values.op = operator;
+    }
+    console.log(values);
+}
+
+/*
+    Design idea for applying calulation
+    use an array with 3 elements
+    take first numbers inputed and record when an operator is used (reset array when clear is used)
+    when operator used, highlight operator, set first index to the number inputed and second to the operator
+    after second number is made and another operator is used, perform operate(args)
+    set new display to that number if 
+
+*/
