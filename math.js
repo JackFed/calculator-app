7a053a0c025482af4c4b1a63021bf9f576b370d0
JackFed <jack.fedurek@gmail.com>
2024-05-29 00:24:48 -0400

    Added display updates with typical calculator effects

--- math.js
+++ math.js
@@ -16,3 +16,37 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
+function changeDisplay(value) {
+    const display = document.querySelector(".display-value");
+    if (display.innerHTML === "0" || (value === "/") || (value === "*") || (value === "+") || (value === "-") 
+        || (display.innerHTML === "/") || (display.innerHTML === "*") || (display.innerHTML === "+") || (display.innerHTML === "-")) {
+        display.innerHTML = value;
+    } else if (value === "C") {
+        display.innerHTML = "0";
+    } else {
+        display.innerHTML = display.innerHTML.concat(value);
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // Add function to check if the button was an operator
+        changeDisplay(button.innerHTML);
+    });
+})
+
