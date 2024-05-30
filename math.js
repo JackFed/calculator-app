a5db97aa02eae2cde83247d97396673ae1897a05
JackFed <jack.fedurek@gmail.com>
2024-05-29 23:20:58 -0400

    Add, sub, divide, mult working

--- math.js
+++ math.js
@@ -1,11 +1,10 @@
 // Globals bad but for now
-let operations = "";
-
 let values = {
-    a: 0,
-    b: 0,
+    firstNum: 0,
     op: "",
-    opId: ""
+    opId: "",
+    waiting: false,
+    equals: false
 }
 
 function add(a, b) {
@@ -42,35 +41,34 @@ function operate(a, operator, b) {
     }
 }
 
-function changeDisplay(value) {
-    const display = document.querySelector(".display-value");
-    const lastChar = operations.charAt(operations.length - 1);
-    if( (value === "/") || (value === "*") || (value === "+") || (value === "-") ) {
-        // IMPLEMENT: if there is already a operator, change it to the new one
-        if( (lastChar === "/") || (lastChar === "*") || (lastChar === "+") || (lastChar === "-") ) {
-            operations = operations.replace(lastChar, value);
-        } else {
-            operations = operations.concat(value);
-        }
-        console.log(`The operations: ${operations}`)
-        display.innerHTML = setValues(display.innerHTML, value);
-    } else if (value === "=") {
-        display.innerHTML = setValues(display.innerHTML, value);
-        operations = display.innerHTML;
-    } else if (value === "C") {
-        display.innerHTML = "0";
-        // reset operation tracking
-        values.a = 0;
-        values.b = 0;
-        values.op = "";
-        operations = "";
-    } else {
-        display.innerHTML = display.innerHTML.concat(value);
-        operations = display.innerHTML;
+function addDigit(oldNum, newD) {
+    if (oldNum === 0) {
+        return newD;
     }
-    console.log(operations)
+    return oldNum * 10 + newD;
 }
 
+function changeDisplay(value) {
+    const display = document.querySelector(".display-value");
+    const digit = Number(value);
+    // If an operator is waitng on a new number
+    if(values.waiting) {
+        values.firstNum = Number(display.innerHTML);
+        display.innerHTML = digit;
+        values.waiting = false;
+    } else {
+        display.innerHTML = addDigit(display.innerHTML, digit);
+    }
+}
+
+function checkExpression() {
+    if (values.op !== "" && values.waiting) {
+
+    }
+}
+
+
+// Buttons
 const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
@@ -79,53 +77,27 @@ numbers.forEach((number) => {
 })
 
 
-
 const operators = document.querySelectorAll(".operator");
 operators.forEach((op) => {
     op.addEventListener("click", () => {
-        if (values.op !== op.innerHTML && values.op !== "") {
+        // If there is already an operator selected, perform the operation.
+        if (values.op !== "") {
             document.querySelector(`#${values.opId}`).classList.remove("press");
+            const display = document.querySelector(".display-value");
+            disVal = Number(display.innerHTML);
+            values.firstNum = operate(values.firstNum, values.op, disVal);
+            display.innerHTML = values.firstNum;
         }
+        // Update operation values
         values.op = op.innerHTML;
         values.opId = op.id;
+        values.waiting = true;
         op.classList.add("press");
-        
-        console.log(values.op)
+        console.log(values)
     });
 })
 
-// Get the values and the operator
-// If it doesn't have any value for a, set a and the operator
-// if it does, set the value of b, perform operate.
-// Then set the new value of a to the result, and set the new operator to the give operator.
-function setValues(value, operator) {
-    let result = value;
-    if (operator === "=") {
-        console.log(values)
-        result = operate(values.a, values.op, Number(value)).toFixed(3);
-        values.a = result;
-        values.op = "";
-    } else if(values.a === 0) {
-        values.a = Number(value);
-        values.op = operator;
-    } else {
-        console.log(`Before equals: ${values}`);
-        values.b = Number(value);
-        result = operate(values.a, values.op, values.b).toFixed(3);
-        values.a = result;
-        values.op = operator;
-    }
-
-    console.log(values);
-    return result
-}
-
-/*
-    Design idea for applying calulation
-    use an array with 3 elements
-    take first numbers inputed and record when an operator is used (reset array when clear is used)
-    when operator used, highlight operator, set first index to the number inputed and second to the operator
-    after second number is made and another operator is used, perform operate(args)
-    set new display to that number if 
-
-*/
+const equal = document.querySelector(".equals");
+equal.addEventListener("click", () => {
+    return 0;
+});
